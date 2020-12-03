6d59f890527eff9503cef3f7e93220e2ccb41dd9
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-03 13:01:50 +0100

    Done

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 4000;
-const publicDomain = process.env.PUBLIC_DOMAIN || 'http://localhost:3000'
+const publicDomain = process.env.PUBLIC_DOMAIN || "http://localhost:3000";
 
-const config = require('./config/db');
+const config = require("./config/db");
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+mongoose
+  // Added parameters to solve warnings on console.
+  .connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => {
+      console.log("Database is connected 🎉");
+    },
+    (err) => {
+      console.log("Can not connect to the database" + err);
+    }
+  );
 
-const todoRoute = require('./routes/todoRoute');
+// Added to solve warnings on console.
+mongoose.set("useFindAndModify", false);
+
+const todoRoute = require("./routes/todoRoute");
 
 app.use(bodyParser.json());
-app.use(cors({
-  credentials: true,
-  origin: [publicDomain]
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: [publicDomain],
+  })
+);
 
-app.use('/api/v1', todoRoute);
+app.use("/api/v1", todoRoute);
 
-const server = app.listen(port, function(){
-  console.log('Listening on port ' + port);
+const server = app.listen(port, function () {
+  console.log("Listening on port " + port + "💻");
 });
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const TodoController = require('../todosController/todos');
+const express = require("express");
+const TodoController = require("../todosController/todos");
 
 const router = express.Router();
 
-router.get('/todos', TodoController.getAllTodos);
-router.get('/todos/:id', TodoController.getTodo);
-router.post('/todos', TodoController.createTodo);
-router.put('/todos/:id', TodoController.updateTodo);
-router.delete('/todos/:id', TodoController.deleteTodo);
+router.get("/todos", TodoController.getAllTodos);
+router.get("/todos/:id", TodoController.getTodo);
+router.post("/todos", TodoController.createTodo);
+router.put("/todos/:id", TodoController.updateTodo);
+router.delete("/todos/:id", TodoController.deleteTodo);
 
 module.exports = router;
